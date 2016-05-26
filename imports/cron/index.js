94589f5d7786438c76e27bb19d3acaad577adf6f
Parminder Klair <p.klair@scorchsoft.com>
2016-05-26 17:13:26 +0100

    added stackable sync

--- imports/cron/index.js
+++ imports/cron/index.js
@@ -1,1 +1,20 @@
-import './sync-skills';
+import { Meteor } from 'meteor/meteor';
+const CronJob = require('cron').CronJob;
+
+import {syncSkills} from './sync-skills.js';
+
+Meteor.methods({
+    'sync.skills'() {
+        syncSkills();
+    }
+});
+
+var job = new CronJob({
+    cronTime: '* * * * *',
+    onTick: function() {
+        console.log('You will see this message every minute');
+        //syncSkills();
+    },
+    start: false
+});
+job.start();
--- imports/cron/sync-skills.js
+++ imports/cron/sync-skills.js
@@ -1,17 +1,40 @@
-const CronJob = require('cron').CronJob;
-const Stackable = require('stackable');
+import Stackable from 'stackable';
+import async from 'async';
+
+import { Skills } from '../api/skills/skills.js';
 
 var stackable = new Stackable('eeFuYBqGA8Qv');
 
+exports.syncSkills = function() {
+    console.log('start sync');
+    let data;
 
-var job = new CronJob({
-    cronTime: '* * * * *',
-    onTick: function() {
-        console.log('You will see this message every minute');
-        stackable.getContainers((error, result) => {
-            console.log(error, result);
-        });
-    },
-    start: false
-});
-job.start();
+    async.series([
+        Meteor.bindEnvironment((callback) => {
+            //get data from stackable
+            stackable.getContainerItems('2LM4yZiRvybxrHSbc', (error, result) => {
+                //console.log(error, result);
+                data = result.data;
+                callback(null);
+            });
+        }),
+        Meteor.bindEnvironment((callback) => {
+            //first remove all docs in db
+            Skills.remove({});
+            callback(null);
+        }),
+        Meteor.bindEnvironment((callback) => {
+            //now insert new
+            async.each(data, (row, callbackEach) => {
+                //console.log(row.data);
+                Skills.insert({
+                    title: row.data.title,
+                    type: row.data.type
+                });
+                callbackEach();
+            }, (err) => {
+                callback(null);
+            });
+        })
+    ]);
+};
